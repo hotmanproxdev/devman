185d1b8522881895066f650e125ded25f8096dcb
eroluysal <eutkuuysal@gmail.com>
2015-12-18 16:34:57 +0200

    last changes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -144,6 +144,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\BaseServiceProvider::class,
+
 
     ],
 
@@ -192,6 +194,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Json'      => Illuminate\Contracts\Routing\ResponseFactory::class,
 
     ],
 
