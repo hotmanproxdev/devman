38847a074b45b143034632a90c4ae358a5aedfe1
eroluysal <eutkuuysal@gmail.com>
2015-12-22 11:17:59 +0200

    pagesetcommand upload

---
+++ app/Console/Commands/pageSetCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Filesystem\Factory;
+
+class pageSetCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'page {page}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command creates a new page';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $app_path="".str_replace("/storage","",storage_path("app"))."/Http/Controllers/";
+        $view_path="".str_replace("/storage/app","",storage_path("app"))."/resources/views/";
+
+
+        mkdir("".$app_path."".config("app.admin_dirname")."/".ucfirst($this->argument("page"))."", 0777, true);
+        chmod("".$app_path."".config("app.admin_dirname")."/".ucfirst($this->argument("page"))."", 0777);
+
+        if(touch("".$app_path."".config("app.admin_dirname")."/".$this->argument("page")."/".$this->argument("page")."Controller.php"))
+        {
+            $dosya = "".storage_path("app")."/test.txt";
+            $dt = fopen($dosya, "rb");
+            $icerik = fread($dt, filesize($dosya));
+
+            $dosya = "".$app_path."".config("app.admin_dirname")."/".$this->argument("page")."/".$this->argument("page")."Controller.php";
+            $dt = fopen($dosya, 'w');
+            $icerik=str_replace("test",$this->argument("page"),$icerik);
+            $icerik=str_replace("__namespace",ucfirst($this->argument("page")),$icerik);
+            fwrite($dt,$icerik);
+            fclose($dt);
+
+
+            mkdir("".$view_path."".config("app.admin_dirname")."/".$this->argument("page")."", 0777, true);
+            chmod("".$view_path."".config("app.admin_dirname")."/".$this->argument("page")."", 0777);
+
+            if(touch("".$view_path."".config("app.admin_dirname")."/".$this->argument("page")."/main.blade.php"))
+            {
+
+                if(touch("".$view_path."".config("app.admin_dirname")."/".$this->argument("page")."/".$this->argument("page")."_content.blade.php"))
+                {
+                    $dosya = "".storage_path("app")."/test_main.txt";
+                    $dt = fopen($dosya, "rb");
+                    $icerik = fread($dt, filesize($dosya));
+
+                    $dosya = "".$view_path."".config("app.admin_dirname")."/".$this->argument("page")."/main.blade.php";
+                    $dt = fopen($dosya, 'w');
+                    fwrite($dt,str_replace("test",$this->argument("page"),$icerik));
+
+
+                    $dosya = "".storage_path("app")."/test_content.txt";
+                    $dt = fopen($dosya, "rb");
+                    $icerik = fread($dt, filesize($dosya));
+
+                    $dosya = "".$view_path."".config("app.admin_dirname")."/".$this->argument("page")."/".$this->argument("page")."_content.blade.php";
+                    $dt = fopen($dosya, 'w');
+                    fwrite($dt,$icerik);
+
+
+                    fclose($dt);
+
+                }
+            }
+
+            dd("File has been created");
+
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
 
         Commands\Inspire::class,
         Commands\Using::class,
+        Commands\pageSetCommand::class,
     ];
 
     /**
