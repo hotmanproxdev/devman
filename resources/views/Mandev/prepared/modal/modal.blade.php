f42e76ab6f1259a463488a714108315a252f9092
eroluysal <eutkuuysal@gmail.com>
2016-01-11 17:34:54 +0200

    modal height

--- resources/views/Mandev/prepared/modal/modal.blade.php
+++ resources/views/Mandev/prepared/modal/modal.blade.php
@@ -3,7 +3,7 @@
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
     <h4 class="modal-title" style="font-weight:bold;">Responsive</h4>
   </div>
-  <div class="modal-body modalLoadresponsive" style="min-height:500px;">
+  <div class="modal-body modalLoadresponsive" style="min-height:700px;">
 
   </div>
 
@@ -15,7 +15,7 @@
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
     <h4 class="modal-title">Stack One</h4>
   </div>
-  <div class="modal-body modalLoadstack1" style="min-height:500px;">
+  <div class="modal-body modalLoadstack1" style="min-height:700px;">
 
   </div>
   <div class="modal-footer">
@@ -31,7 +31,7 @@
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
     <h4 class="modal-title">Full Width</h4>
   </div>
-  <div class="modal-body modalLoadfull-width" style="min-height:500px;">
+  <div class="modal-body modalLoadfull-width" style="min-height:700px;">
 
   </div>
 </div>
