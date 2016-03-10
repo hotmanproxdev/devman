7c4139bcd3b58ca2920142f424e96aea637e519e
eroluysal <eutkuuysal@gmail.com>
2016-03-10 14:18:54 +0200

    appapa

--- config/app.php
+++ config/app.php
@@ -170,7 +170,7 @@ return [
         ViKon\DbExporter\DbExporterServiceProvider::class,
         Orangehill\Iseed\IseedServiceProvider::class,
         JeroenG\Packager\PackagerServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+        //Intervention\Image\ImageServiceProvider::class,
 
 
     ],
@@ -240,7 +240,7 @@ return [
         'ApiServices' =>App\Http\Services\apiServicesController::class,
         'Filter' =>App\Http\Services\filterController::class,
         'Role' =>App\Http\Services\pageRoleController::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        //'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
